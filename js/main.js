317aad23d80458cf151cd15e95624da933ddcebd
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-06-02 15:54:09 -0600

    Create displayController module and drawChoice function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@ const player = (name, points, wins) => {
     return {name, points, wins};
 };
 
+const displayController = (() => {
+    let counter = 0;
+    const gameCells = document.querySelectorAll('.gameCell');
+    const drawChoice = (e) => {
+        let choice = document.createElement('div');
+        if (e.target.textContent !== '') return;
+        if(counter % 2){
+            choice.textContent = 'O'
+        } else choice.textContent = 'X'
+        e.target.appendChild(choice);
+        counter++
+    };
+    gameCells.forEach(x => x.addEventListener('click', drawChoice))
+})();
+
 const gameBoard = (() => {
     const gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     return {
