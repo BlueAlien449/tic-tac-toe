d6fe99ff8cfac1537a41d2ace1ccde665c44c4a5
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-06-02 18:47:36 -0600

    Add datasets to gameboard divs in html and create checkWinner function in js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,16 @@ const displayController = (() => {
     let counter = 0;
     const gameCells = document.querySelectorAll('.gameCell');
     const drawChoice = (e) => {
+        let index = e.target.dataset;
         let choice = document.createElement('div');
         if (e.target.textContent !== '') return;
         if(counter % 2){
             choice.textContent = 'O'
-        } else choice.textContent = 'X'
+            gameBoard.gameArray[index.cellnumber] = -1;
+        } else {
+            choice.textContent = 'X'
+            gameBoard.gameArray[index.cellnumber] = 1;
+        }
         e.target.appendChild(choice);
         counter++
     };
@@ -18,8 +23,28 @@ const displayController = (() => {
 })();
 
 const gameBoard = (() => {
-    const gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const gameArray = [0,1,2,3,4,5,6,7,8];
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+    ];
+    const checkWinner = () => {
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (gameArray[a] && gameArray[a] === gameArray[b] && gameArray[a] === gameArray[c]) {
+                return gameArray[a]; // Return the winning player symbol (1 for 'X' or -1 for 'O')
+            }
+        }
+        return null; // If there is no winner
+    }
     return {
-        gameArray
+        gameArray,
+        checkWinner
     };
 })();
